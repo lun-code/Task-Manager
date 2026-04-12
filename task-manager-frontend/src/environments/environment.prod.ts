053f53730082a4739c915af89276c24bd736f738
lun-code <lun.code01@gmail.com>
2026-04-12 12:02:59 +0200

    fix: url

--- task-manager-frontend/src/environments/environment.prod.ts
+++ task-manager-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://task-manager-production-8226.up.railway.app'
+  apiUrl: 'https://task-manager-production-e59e.up.railway.app'
 };
