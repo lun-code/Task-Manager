189fff8f0431409f2cde783ef92c64ef8ec9596c
lun-code <lun.code01@gmail.com>
2026-04-13 13:54:50 +0200

    Cambios properties

--- task-manager-api/src/main/java/com/example/task_manager_api/service/EmailService.java
+++ task-manager-api/src/main/java/com/example/task_manager_api/service/EmailService.java
@@ -3,19 +3,30 @@ package com.example.task_manager_api.service;
 import com.resend.Resend;
 import com.resend.core.exception.ResendException;
 import com.resend.services.emails.model.CreateEmailOptions;
-import com.resend.services.emails.model.CreateEmailResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
-    public void sendVerificationEmail(String userEmail, String token) throws ResendException {
-        Resend resend = new Resend("re_LizEknLZ_8wivDtqWQtW9RwXLvzgVuzNQ");
+
+    @Value("${resend.api-key}")
+    private String apiKey;
+
+    public void sendVerificationEmail(String userEmail, String token) {
+        Resend resend = new Resend(apiKey);
 
         CreateEmailOptions params = CreateEmailOptions.builder()
                 .from("onboarding@resend.dev")
                 .to(userEmail)
-                .subject("Verificación")
-                .html("<p>Haz click en el enlace para confirmar tu cuenta: \n\n</p>" + "<p>https://task-manager-nto7.vercel.app/verify?token=</p>" + token)
+                .subject("Verifica tu cuenta")
+                .html("<p>Haz clic en el siguiente enlace para verificar tu cuenta:</p>" +
+                        "<a href='https://task-manager-nto7.vercel.app/verify?token=" + token + "'>Verificar cuenta</a>")
                 .build();
 
-        CreateEmailResponse data = resend.emails().send(params);
+        try {
+            resend.emails().send(params);
+        } catch (ResendException e) {
+            throw new RuntimeException("Error al enviar el email", e);
+        }
     }
 }
