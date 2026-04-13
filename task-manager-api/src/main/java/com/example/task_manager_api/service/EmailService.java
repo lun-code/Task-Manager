c58c972bb1e7b34cdbd60c86df99f66c62a2ff78
lun-code <lun.code01@gmail.com>
2026-04-13 15:44:38 +0200

    Ajustes email

--- task-manager-api/src/main/java/com/example/task_manager_api/service/EmailService.java
+++ task-manager-api/src/main/java/com/example/task_manager_api/service/EmailService.java
@@ -1,23 +1,51 @@
 package com.example.task_manager_api.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private static final String BREVO_API_URL = "https://api.brevo.com/v3/smtp/email";
+    @Value("${BREVO_API_KEY}")
+    private String apiKey;
 
     public void sendVerificationEmail(String to, String token) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("Verifica tu cuenta");
-        message.setText("Haz clic en el siguiente enlace para verificar tu cuenta:\n\n"
-                + "http://localhost:8080/api/auth/verify?token=" + token);
 
-        mailSender.send(message);
+        String verificationUrl = "https://task-manager-production-ccc6.up.railway.app/api/auth/verify?token=" + token;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("api-key", apiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("sender", Map.of(
+                "email", "lun.code01@gmail.com",
+                "name", "Task Manager"
+        ));
+
+        body.put("to", List.of(
+                Map.of("email", to)
+        ));
+
+        body.put("subject", "Verifica tu cuenta");
+
+        body.put("htmlContent",
+                "<p>Haz clic en el siguiente enlace para verificar tu cuenta:</p>" +
+                        "<a href=\"" + verificationUrl + "\">Verificar cuenta</a>"
+        );
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+
+        restTemplate.postForEntity(BREVO_API_URL, request, String.class);
     }
 }
